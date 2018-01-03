ea974119770e95e141b067cb85ccfbc05b0d0275
Ivan Pirlik <abmajor7@gmail.com>
2018-01-03 15:34:24 +0100

    Update release script

--- release.sh
+++ release.sh
@@ -10,7 +10,7 @@ git diff-index --quiet --cached HEAD -- || die "Index dirty, commit first"
 go generate
 git add VERSION
 git add cli/version.go
-git commit -m "Version $VERSION"
+git commit -m "Version $VERSION" || echo "Version not changed"
 git tag -a v$VERSION -m "Version $VERSION"
 git push origin v$VERSION
 goreleaser
